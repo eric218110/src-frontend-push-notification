d13cc0b64c38796ebcc99e877bacc1e2080b8ba8
Eric Silva <ericsilvaccp@gmail.com>
2023-03-11 15:46:11 -0300

    fix: build

--- src/presentation/store/features/web-push-settings/web-push-adapter.ts
+++ src/presentation/store/features/web-push-settings/web-push-adapter.ts
@@ -1,23 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
-import { listApplicationByIdService } from '@services/http/application/list/byId'
-import { listWebPushSettingsInApplicationById } from '@services/http/webPushSettings/list'
 import { State } from './web-push-state'
 
 export const fetchWebPushSettingsAndApplication = createAsyncThunk(
   'fetchWebPushSettingsAndApplication',
   async (appId: number): Promise<Omit<State, 'isLoading'>> => {
-    const application = await listApplicationByIdService(appId)
-    const webPushSettings = await listWebPushSettingsInApplicationById(appId)
-
-    if (application.data && webPushSettings.data) {
-      return {
-        data: {
-          ...webPushSettings.data,
-          ...application.data
-        }
-      }
-    }
-
+    console.log(appId)
     throw new Error('Not possible read data')
   }
 )
